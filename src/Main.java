b78a146b46c1eec39fdd65619c8a4a45c1a0187a
hudea <hudealex1@gmail.com>
2025-10-21 12:38:36 +0300

    aufgabe 3, 4 +Main

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,21 @@ public class Main {
         int[] noten = {29, 37, 38, 41, 84, 67};
         System.out.println(Arrays.toString(noten));
 
+        //1
         int[] ausreichendNoten = NotenBehandlung.getAusreichendNoten(noten);
         System.out.println("Ausreichende Noten:" + Arrays.toString(ausreichendNoten));
 
+        //2
         double durchschnitt = NotenBehandlung.berechneNotendurchschnitt(noten);
         System.out.println("Notendurchschnitt: " + durchschnitt);
+
+        //3
+        int[] runden = NotenBehandlung.getrundeNoten(noten);
+        System.out.println("Runde Note: " + Arrays.toString(runden));
+
+        //4
+        System.out.println("Maximal Runden Note: " + NotenBehandlung.getMaximalGerundeteNote(runden));
+
+
     }
 }
--- src/NotenBehandlung.java
+++ src/NotenBehandlung.java
@@ -32,7 +32,40 @@ public class NotenBehandlung {
         return (double) summe / noten.length;
     }
 
+    private static int rundeNote(int note) {
+        if (note < 38) {
+            return note;
+        }
 
+        int rest = note % 5;
 
+        if (rest >= 3) {
+            return note + (5 - rest);
+        } else {
+            return note;
+        }
+    }
+
+    public static int[] getrundeNoten(int[] noten) {
+        int[] gerundeteNoten = new int[noten.length];
+        for (int i = 0; i < noten.length; i++) {
+            gerundeteNoten[i] = rundeNote(noten[i]);
+        }
+        return gerundeteNoten;
+    }
+
+    public static int getMaximalGerundeteNote(int[] noten) {
+        int[] gerundete = getrundeNoten(noten);
+        if (gerundete.length == 0) {
+            return -1;
+        }
+        int maxNote = 0;
+        for (int i = 0; i < gerundete.length; i++) {
+            if (maxNote < gerundete[i]) {
+                maxNote = gerundete[i];
+            }
+        }
+        return maxNote;
+    }
 
 }
